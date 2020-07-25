a8df67fbd75838bd96c4fd51a4d2f1a576aa39a7
Shivansh Sharma <shivanshsharma242@gmail.com>
2020-07-25 13:32:59 +0530

    Update and rename Matrix Chain Multiplication to Matrix Chain Multiplication.java

---
+++ Matrix Chain Multiplication.java
@@ -0,0 +1,3 @@
+/* Given an array p[] which represents the chain of matrices such that the ith matrix Ai is of dimension p[i-1] x p[i]. 
+We need to write a function MatrixChainOrder() that should return the minimum number of multiplications needed to multiply the chain. */
+
