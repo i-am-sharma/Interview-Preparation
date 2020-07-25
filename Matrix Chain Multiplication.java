c9eae061a8d084bfea40c6d3715a5bc5e962fa4c
Shivansh Sharma <shivanshsharma242@gmail.com>
2020-07-25 13:34:36 +0530

    Update Matrix Chain Multiplication.java
    code for MCM

--- Matrix Chain Multiplication.java
+++ Matrix Chain Multiplication.java
@@ -1,3 +1,5 @@
+// Problem Statement :
+
 /* Given an array p[] which represents the chain of matrices such that the ith matrix Ai is of dimension p[i-1] x p[i]. 
 We need to write a function MatrixChainOrder() that should return the minimum number of multiplications needed to multiply the chain. */
 
