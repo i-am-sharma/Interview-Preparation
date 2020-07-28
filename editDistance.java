5180e593c6f34b14809aa7ac0ba2a0f15151cd59
Shivansh Sharma <shivanshsharma242@gmail.com>
2020-07-28 22:41:13 +0530

    Update editDistance.java

--- editDistance.java
+++ editDistance.java
@@ -1,1 +1,22 @@
+/* Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) 
+required to convert ‘str1’ into ‘str2’.
 
+Insert
+Remove
+Replace
+All of the above operations are of equal cost. */
+
+
+public class editDistance {
+
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+
+        String s1, s2;
+
+        s1 = sc.next();
+        s2 = sc.next();
+
+        System.out.println("hi");
+    }
+}
