dd04635b300c07b27546e9c618b675fdfa42241b
Shivansh Sharma <shivanshsharma242@gmail.com>
2020-07-29 12:33:38 +0530

    Dynamic Programming

--- matrixChainMultiplication.java
+++ matrixChainMultiplication.java
@@ -1,6 +1,7 @@
-/* Given an array p[] which represents the chain of matrices such that the ith matrix Ai is of dimension p[i-1] x p[i]. 
+/* Problem Statement - Given an array p[] which represents the chain of matrices such that the ith matrix Ai is of dimension p[i-1] x p[i]. 
 We need to write a function MatrixChainOrder() that should return the minimum number of multiplications needed to multiply the chain. */
 
+// Code by @shivshar
 import java.util.*;
 
 public class matrixChainMultiplication {
