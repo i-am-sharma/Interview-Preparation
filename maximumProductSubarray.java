28ded5211a3683b86c09941a3f392e5400495666
Shivansh Sharma <shivanshsharma242@gmail.com>
2020-07-29 12:34:19 +0530

    Dynamic Programming

--- maximumProductSubarray.java
+++ maximumProductSubarray.java
@@ -1,6 +1,7 @@
-/* Given an array that contains both positive and negative integers, find the product of the maximum product subarray. 
+/* Problem Statement - Given an array that contains both positive and negative integers, find the product of the maximum product subarray. 
 Expected Time complexity is O(n) and only O(1) extra space can be used. */
 
+// Code by @shivshar
 import java.util.*;
 
 public class maximumProductSubarray {
@@ -44,5 +45,4 @@ public class maximumProductSubarray {
         }
         sc.close();
     }
-
 } 
