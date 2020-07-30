ea0ce7177c9f5204a55968251b1b7348bd194111
Shivansh Sharma <shivanshsharma242@gmail.com>
2020-07-30 14:13:32 +0530

    Update longestIncresaingSubsequence.java

--- longestIncresaingSubsequence.java
+++ longestIncresaingSubsequence.java
@@ -3,3 +3,13 @@ For example, the length of LIS for {10, 22, 9, 33, 21, 50, 41, 60, 80} is 6 and 
 
 // Code by @shivshar
 import java.util.*;
+
+class Solution {
+    public static void main (String[] args) {
+        //testcases
+        Scanner sc = new Scanner (System.in);
+        int T = sc.next;
+        
+
+    }
+}
