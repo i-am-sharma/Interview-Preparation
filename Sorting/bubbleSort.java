8f4cf0b7b4751b9e1b64866265d2945036f0e847
Shivansh Sharma <shivanshsharma242@gmail.com>
2020-08-31 16:42:37 +0530

    Update bubbleSort.java

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -1,19 +1,23 @@
 package Sorting;
 import java.util.*;
+
 public class bubbleSort {
     public static int[] sorting (int[] arr, int n){
         // Two key point in bubble sort -
         // 1. We compare adjacent elements
         // 2. bigger number will resulted in last in each iteration
-
+        boolean swapped = false;
         for(int i=0; i<n; i++){
             for(int j=0; j<n-i-1; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
             }
+            // List is already sorted
+            if (swapped == false) break;
         }
         return arr;
     }
@@ -35,4 +39,6 @@ public class bubbleSort {
 
 }
 
-// Time Complexity - O(n^2)
+// Worst Case Time Complexity - O(n^2)
+// Best Case Time Complexity - O(n) when list is already sorted
+// Space complexity - O(1)
