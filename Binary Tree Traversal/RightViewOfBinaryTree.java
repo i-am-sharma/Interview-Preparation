99827dd3102e6939bb481fa824d3c1b82a9a3439
Shivansh Sharma <shivanshsharma242@gmail.com>
2020-09-21 22:50:49 +0530

    Update RightViewOfBinaryTree.java

--- Binary Tree Traversal/RightViewOfBinaryTree.java
+++ Binary Tree Traversal/RightViewOfBinaryTree.java
@@ -1,4 +1,45 @@
+/* Print Right View of a Binary Tree */
+// Given a Binary Tree, print Right view of it. Right view of a Binary Tree is set of nodes visible when tree is visited from Right side.
+
+// Code by @shivshar
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode () {}
+    TreeNode (int val) {this.val = val;}
+    TreeNode (int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 public class RightViewOfBinaryTree {
-    
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()){
+            int n = q.size();
+            for (int i=0; i<n; i++){
+                TreeNode curr = q.poll();
+                if (i == n-1) ans.add(curr.val);
+                if (curr.left != null) q.offer(curr.left);
+                if (curr.right != null) q.offer(curr.right);
+            }
+        }
+        return ans;
+    }
 }
+
+
+// Time complexity : O(n)'
+// Space complexity : O(n)
+
+
+
