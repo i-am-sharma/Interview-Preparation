7e257bc0ad36613fff4f715e380e125486103e5b
Shivansh Sharma <shivanshsharma242@gmail.com>
2020-09-22 13:45:52 +0530

    Update TopViewOfBinaryTree.java

--- Binary Tree Traversal/TopViewOfBinaryTree.java
+++ Binary Tree Traversal/TopViewOfBinaryTree.java
@@ -24,7 +24,7 @@ class Pair<U, V> {
         this.first = first;
         this.second = second;
     }
-
+    // Factory method for creating a Typed Pair immutable instance
     public static <U, V> Pair<U, V> of(U a, V b) {
         return new Pair<>(a, b);
     }
